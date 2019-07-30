593df975b541ef2df8867b3e8ca5113a08295cb9
oalmalky5 <oalmalky5@gmail.com>
2019-07-30 16:24:11 +0300

    fix slash

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const expressPort = 3000
 const apiUrls = {
-  production: 'https://quiet-peak-51750.herokuapp.com/',
+  production: 'https://quiet-peak-51750.herokuapp.com',
   development: `http://localhost:${expressPort}`
 }
 
